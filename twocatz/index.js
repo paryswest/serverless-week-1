a77676ac9bfae2b2ea329b96bd3435f7c72250f8
Parys West <paryswest@gmail.com>
2021-06-23 23:25:29 -0400

    added fetch v2

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,16 +3,21 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless" , {
+    let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject" , {
         method: 'GET'
     });
     
     let data = await resp.arrayBuffer()
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+
+    var base64data = Buffer.from(data).toString('base64')
+    //put what you want to turn into base64 inside "originaldata"
+    //"originaldata" will be encoded in base64.
+
     
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: base64data
     };
 }

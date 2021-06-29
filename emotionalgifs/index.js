24376eff0f2f64af67bc28b9812c72e3d30dc6a8
Parys West <paryswest@gmail.com>
2021-06-29 15:54:42 -0400

    add face api fetch call

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,24 +1,53 @@
 var multipart = require('parse-multipart');
+var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-   // here's your boundary:
-   var boundary = multipart.getBoundary(req.headers['content-type']);
-  
-   // TODO: assign the body variable the correct value
-   var body = req.body
+  // here's your boundary:
+  var boundary = multipart.getBoundary(req.headers['content-type']);
 
-   // parse the body
-   var parts = multipart.Parse(body, boundary);
+  // TODO: assign the body variable the correct value
+  var body = req.body;
 
-   var imageData = parts[0].data
+  // parse the body
+  var parts = multipart.Parse(body, boundary);
 
-   var convertedResult = Buffer.from(imageData).toString('base64');
-   // FILL IN THE BLANK
+  var imageData = parts[0].data;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
-    };
+  //module.exports function
+  //analyze the image
+  var result = await analyzeImage(imageData);
+  context.res = {
+    body: {
+      result,
+    },
+  };
+  console.log(result);
+  context.done();
+};
+
+async function analyzeImage(img) {
+  const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+  const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+  let params = new URLSearchParams({
+    returnFaceId: 'true',
+    returnFaceAttributes: 'emotion', //FILL IN THIS LINE
+  });
+
+  //COMPLETE THE CODE
+  let resp = await fetch(uriBase + '?' + params.toString(), {
+    method: 'POST', //WHAT TYPE OF REQUEST?
+    body: img, //WHAT ARE WE SENDING TO THE API?
+
+    //ADD YOUR TWO HEADERS HERE
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Ocp-Apim-Subscription-Key': subscriptionKey,
+    },
+  });
+
+  let emotionData = await resp.json();
+  return emotionData;
 }

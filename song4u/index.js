15d442cabd02e1241ff08136695ff1a2212dc961
Parys West <paryswest@gmail.com>
2021-07-02 21:57:32 -0400

    waiting for a wing on a prayer 2

--- song4u/index.js
+++ song4u/index.js
@@ -5,19 +5,19 @@ module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   const queryObject = querystring.parse(req.body);
-
+  context.log(queryObject);
   let url = queryObject.MediaUrl0;
-
+  context.log(url);
   let resp = await fetch(url, {
     method: 'GET',
   });
 
   let data = await resp.arrayBuffer();
 
-  var result = analyzeImage(data);
-
+  var result = await analyzeImage(data);
+  context.log(result);
   let age = result[0].faceAttributes.age;
-
+context.log(age)
   let id = '';
 
   if (age > 5 && age < 25) {
@@ -31,7 +31,7 @@ module.exports = async function (context, req) {
   } else {
     id = 'Unknown';
   }
-
+context.log(id)
   context.res = {
     // status: 200, /*Defaults to 200 */
     body: id,
@@ -39,12 +39,12 @@ module.exports = async function (context, req) {
 };
 
 async function analyzeImage(img) {
-//   const subscriptionKey = process.env.SUBSCRIPTION_KEY;
-//   const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+  //   const subscriptionKey = process.env.SUBSCRIPTIONKEY2;
+  //   const uriBase = process.env.ENDPOINT2 + '/face/v1.0/detect';
 
-const subscriptionKey = "a6d27d537aae4a4f965954450644a240"
-const uriBase = "https://shogomakishimaxx.cognitiveservices.azure.com/face/v1.0/detect"
-
+  const subscriptionKey = 'a6d27d537aae4a4f965954450644a240';
+  const uriBase =
+    'https://shogomakishimaxx.cognitiveservices.azure.com/face/v1.0/detect';
 
   let params = new URLSearchParams({
     returnFaceId: 'true',
@@ -52,6 +52,7 @@ const uriBase = "https://shogomakishimaxx.cognitiveservices.azure.com/face/v1.0/
   });
 
   //COMPLETE THE CODE
+  console.log(uriBase + '?' + params.toString());
   let resp = await fetch(uriBase + '?' + params.toString(), {
     method: 'POST', //WHAT TYPE OF REQUEST?
     body: img, //WHAT ARE WE SENDING TO THE API?

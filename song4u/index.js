ef1ec904059184caf24da7777d9793ff5215ecde
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-03 23:33:12 -0400

    ignore, fixing the bot

--- song4u/index.js
+++ song4u/index.js
@@ -59,6 +59,7 @@ async function analyzeImage(img) {
     headers: {
       'Content-Type': 'application/octet-stream',
       'Ocp-Apim-Subscription-Key': subscriptionKey,
+      // This is where you authenticate the API request!
     },
   });
 

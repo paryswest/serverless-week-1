8fa42217a3eac7b4e6b4ef663049a56a5d114dfa
Parys West <paryswest@gmail.com>
2021-07-03 20:29:31 -0400

    song4u edit again

--- song4u/index.js
+++ song4u/index.js
@@ -69,12 +69,12 @@ async function analyzeImage(img) {
 
 function retrieveSong(gener) {
   const songs = {
-    GenZ: 'https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf',
-    GenY: 'https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9',
-    GenX: 'https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d',
-    BabyBoomers:
+    "GenZ": 'https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf',
+    "GenY": 'https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9',
+    "GenX": 'https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d',
+    "BabyBoomers":
       'https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50',
-    Unknown:
+    "Unknown":
       'https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11',
   };
 

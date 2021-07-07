cd5434c7294ee958fa71b1d49b0b3f98fa330b18
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-06 23:24:25 -0400

    fixing repo

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,5 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    
+    // starter template
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name

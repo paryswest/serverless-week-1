1678be94a69b33a2d3e9b2499f8242456c76bcac
Parys West <paryswest@gmail.com>
2021-07-16 11:42:25 -0400

    please work

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,8 +1,34 @@
 function getImage(event) {
   event.preventDefault();
-  if (document.getElementById('name').value != '') {
-    $('#output').text('Thanks!');
+  var myform = document.getElementById('myform');
+  //get image and filename uploaded by user via the form
+  let nameInput = document.getElementById('username');
+  let fileInput = document.getElementById('image');
+  let file = fileInput.files[0]; //get image data
+
+  var payload = new FormData(myform);
+  console.log(payload);
+  payload.append('file', file);
+  $('#output').text('Thanks!');
+
+  if (document.getElementById('username').value != '') {
+    try {
+      let url = 'https://hackervoicee.azurewebsites.net/api/bunnimage-upload';
+      console.log('Image was uploaded, making POST request to Azure function');
+      // create request to Azure functions
+      const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          "codename": nameInput.value
+        },
+        body: payload,
+      });
+
+      $('#output').text('Your image has been stored successfully!');
+    } catch (err) {
+      $('#output').text(err);
+    }
   } else {
-    alert("No name error.");
+    alert('No name error.');
   }
 }

01fad5dd62079d1a0087b6e08bcc618e60aa0e84
Parys West <paryswest@gmail.com>
2021-07-18 17:14:03 -0400

    committing code

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,28 +1,28 @@
 function y1k3s() {
     let name1 = document.getElementById("name1").value
 
-    let endpoint1 = "data:image/png;base64" + "https://cataas.com/cat/cute/says/" + name1
+    let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
     if(name1 != '') {
         document.getElementById("image1").src = endpoint1 //call to API
     }
 
     let name2 = document.getElementById("name2").value
 
-    let endpoint2 = "data:image/png;base64" + "https://cataas.com/cat/cute/says/" + name2
+    let endpoint2 = "https://cataas.com/cat/cute/says/" + name2
     if(name2 != '') {
         document.getElementById("image2").src = endpoint2 //call to API
     }
 
     let name3 = document.getElementById("name3").value
 
-    let endpoint3 = "data:image/png;base64" + "https://cataas.com/cat/cute/says/" + name3
+    let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
     if(name3 != '') {
         document.getElementById("image3").src = endpoint3 //call to API
     }
 
     let name4 = document.getElementById("name4").value
 
-    let endpoint4 = "data:image/png;base64" + "https://cataas.com/cat/cute/says/" + name4
+    let endpoint4 =  "https://cataas.com/cat/cute/says/" + name4
     if(name4 != '') {
         document.getElementById("image4").src = endpoint4 //call to API
     }

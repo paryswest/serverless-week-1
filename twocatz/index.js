e82fc7d96d47e3cc8cdb84761965039af1fda6e5
Parys West <paryswest@gmail.com>
2021-07-18 00:10:53 -0400

    week 4 step 5

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,44 +3,47 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    async function getCat() {
-        let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject" , {
+    let name1 = req.query.name1
+    let name2 = req.query.name2
+    let name3 = req.query.name3
+    let name4 = req.query.name4
+
+    async function getCat(name) {
+        let endpoint = "https://cataas.com/cat/cute/says/" + name
+        let resp = await fetch(endpoint, {
             method: 'GET'
         });
-        
-        let data = await resp.arrayBuffer()
-        // we need to receive it as a buffer since this is an image we are receiving from the API
-        // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-
-        var base64data = Buffer.from(data).toString('base64')
-        //put what you want to turn into base64 inside "originaldata"
-        //"originaldata" will be encoded in base64.
-
-        return base64data
-    }
-
-    let catpic1 = await getCat()
-    let catpic2 = await getCat()
-
-
-    function getNames() {
-        var names = ["Shreya", "Emily", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
-        var random_value = Math.floor(names.length * Math.random())
-        var resultName = names[random_value]
-
-        return resultName
-    }
-
-    let name1 = getNames()
-    let name2 = getNames()
-
     
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            cat1: catpic1,
-            cat2: catpic2,
-            names: [name1, name2]
-        }
-    };
-}
+        let data = await resp.arrayBuffer()
+        let base64data = Buffer.from(data).toString('base64')
+
+        return base64data;
+    }
+
+    let cat1 = await getCat(name1)
+    let cat2 = await getCat(name2)
+    let cat3 = await getCat(name3)
+    let cat4 = await getCat(name4)
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                cat1: cat1,
+                cat2: cat2,
+                cat3: cat3,
+                cat4: cat4,
+            }
+        };
+  }
+    // function getNames() {
+    //     var names = ["Shreya", "Emily", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+    //     var random_value = Math.floor(names.length * Math.random())
+    //     var resultName = names[random_value]
+
+    //     return resultName
+    // }
+
+    // let name1 = getNames()
+    // let name2 = getNames()
+
+  

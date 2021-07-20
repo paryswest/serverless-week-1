dc8632da8665bf2ebc64bbf7145e78f04c52e8b3
Parys West <paryswest@gmail.com>
2021-07-19 22:46:51 -0400

    changes to index.js file

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,28 +1,36 @@
-function y1k3s() {
+async function y1k3s() {
+    let url = `https://hackervoicee.azurewebsites.net/api/twocatz?code=ERfyT2jJ10MzWeAf0DqQutaJnB80I2nCtYcNFnEZa2LycnHAKXRjrg==`
+
+    let resp = await fetch(url, {
+        method: 'GET'
+    });
+
+    let data = await resp.json();
+
     let name1 = document.getElementById("name1").value
 
-    let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
+    let endpoint1= "data:image/png;base64," + data.cat1
     if(name1 != '') {
         document.getElementById("image1").src = endpoint1 //call to API
     }
 
     let name2 = document.getElementById("name2").value
 
-    let endpoint2 =  "https://cataas.com/cat/cute/says/" + name2
+    let endpoint2= "data:image/png;base64," + data.cat2
     if(name2 != '') {
         document.getElementById("image2").src = endpoint2 //call to API
     }
 
     let name3 = document.getElementById("name3").value
 
-    let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
+    let endpoint3= "data:image/png;base64," + data.cat3
     if(name3 != '') {
         document.getElementById("image3").src = endpoint3 //call to API
     }
 
     let name4 = document.getElementById("name4").value
 
-    let endpoint4 = "https://cataas.com/cat/cute/says/" + name4
+    let endpoint4= "data:image/png;base64," + data.cat4
     if(name4 != '') {
         document.getElementById("image4").src = endpoint4 //call to API
     }

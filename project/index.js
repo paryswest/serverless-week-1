dc4af61f280a9fda2d41da1bc9e05ff66ccbd056
Parys West <paryswest@gmail.com>
2021-08-06 12:10:54 -0400

    integrating project

--- project/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector ('.button')
-const inputValue = document.querySelector ('.input')
-const name = documnent.querySelector('.name');
-const desc = documnent.querySelector('.desc');
-const temp = documnent.querySelector('.temp');
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&APPID=b603af9acd96c879fb647ff394b51ead")
-
